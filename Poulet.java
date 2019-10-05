708c1a174d4db8361879887080798fcd20ca9464
georges.nordon <georges.nordon@free.fr>
2019-10-05 12:55:33 +0200

    ajout de tous les fichiers à l'index

---
+++ Poulet.java
@@ -0,0 +1,13 @@
+import p1.*;
+public class Poulet extends Volaille {
+	String id = "";
+	double poids = 0;
+	public Poulet(String pId, double pPoids) {
+		super( pId, pPoids);
+	}
+
+/*	public void afficher() {
+		System.out.println("Identification du poulet : " + id + "  " + "Poids : " + poids);
+	}
+*/
+}//Fin class Poulet
